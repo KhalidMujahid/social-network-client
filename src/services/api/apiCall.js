7cf59cde605fb0d4fb139263976f745904de0f1a
KhalidMujahid <binkhalid267@gmail.com>
2023-10-08 00:35:28 +0100

    changes to apiCall.js

--- src/services/api/apiCall.js
+++ src/services/api/apiCall.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiCall = axios.create({
-  baseURL: import.meta.env.VITE_NODE_ENV === "development" ? import.meta.env.VITE_BASE_URL : import.meta.env.GLOBAL_URL,
+  baseURL: import.meta.env.VITE_NODE_ENV === "development" ? import.meta.env.VITE_BASE_URL : import.meta.env.VITE_GLOBAL_URL,
 });
 
 export default apiCall;
