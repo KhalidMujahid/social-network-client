46c03ee9d70a17dd94360c5fd00c50617a26df17
KhalidMujahid <binkhalid267@gmail.com>
2023-10-08 00:58:24 +0100

    another commit

--- src/services/constant/baseURL.js
+++ src/services/constant/baseURL.js
@@ -1,2 +1,2 @@
-export const host = "http://localhost:3001";
-export const base = "http://localhost:3001/images";
+export const host = import.meta.env.VITE_NODE_ENV === "development" ? "http://localhost:3001" : import.meta.env.VITE_GLOBAL_URL;
+export const base = import.meta.env.VITE_NODE_ENV === "development" ? "http://localhost:3001/images" : `${import.meta.env.VITE_GLOBAL_URL}/images`;
